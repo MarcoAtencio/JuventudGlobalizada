14e9a95a7d00a92ebcc2694b9101509952ad76d7
MarcoAtencio <n00049623@upn.pe>
2020-05-02 09:06:49 -0500

    deploy9

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 php artisan migrate:fresh 
 php artisan db:seed
 php artisan tinker 
-use Spatie\Permission\Models\Role;
-$role = Role::create(['name' => 'SuperAdmin']);
-$user = User::find(1);
-$user->assignRole('SuperAdmin');
-exit
